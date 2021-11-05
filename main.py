50ddc8faa0daf5544e3ca7823e032b8befa43284
khadada <lkhadada@gmail.com>
2021-11-05 08:10:24 +0000

    I create my flowchart for this game using drawio software

---
+++ main.py
@@ -0,0 +1,12 @@
+# TODO: 01
+# import the modules that we need for this project
+# ------------------------------------------------
+import random
+import game_data
+import art
+
+# TODO: 02
+# print the logo of the game
+# ------------------------------------------------
+
+print(art.logo)
